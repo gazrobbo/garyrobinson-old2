b833561586ecf04b7dfef6ffe6a5a8dbd17e90a3
Gary Robinson <gaz_rob@hotmail.com>
2019-01-29 19:54:35 +0000

    responsive ready

--- script.js
+++ script.js
@@ -7,14 +7,19 @@ const box2 = document.getElementById ('box2');
 box1.onclick = function() { 
     largeBox[0].className = 'small';
     box1.className = 'large';
-}
+};
 
 box2.onclick = function() { 
     largeBox[0].className = 'small';
     box2.className = 'large';
-}
+};
 
 box3.onclick = function() { 
     largeBox[0].className = 'small';
     box3.className = 'large';
-}
+};
+
+box4.onclick = function() { 
+    largeBox[0].className = 'small';
+    box4.className = 'large';
+};
