c6303108508b4b1d5fd0038acc3c3c1953af6d74
Gary Robinson <gaz_rob@hotmail.com>
2019-01-29 18:23:36 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let smallBox = document.getElementsByClassName('small');
+let largeBox = document.getElementsByClassName('large');
+const box1 = document.getElementById ('box1');
+const box2 = document.getElementById ('box2');
+
+
+box1.onclick = function() { 
+    largeBox[0].className = 'small';
+    box1.className = 'large';
+}
+
+box2.onclick = function() { 
+    largeBox[0].className = 'small';
+    box2.className = 'large';
+}
+
+box3.onclick = function() { 
+    largeBox[0].className = 'small';
+    box3.className = 'large';
+}
