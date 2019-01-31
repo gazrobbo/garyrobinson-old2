b2eea0cd1b6c1f9b3868ceea99009c20f896b9a5
Gary Robinson <gaz_rob@hotmail.com>
2019-01-31 21:04:43 +0000

    auto colour change

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 let smallBox = document.getElementsByClassName('small');
 let largeBox = document.getElementsByClassName('large');
-const box1 = document.getElementById ('box1');
-const box2 = document.getElementById ('box2');
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+let mainColor = document.documentElement;
 
 
-box1.onclick = function() { 
+
+box1.onclick = function () {
     largeBox[0].className = 'small';
     box1.className = 'large';
+    mainColor.style.setProperty('--main-colour', '#23538a');
+    mainColor.style.setProperty('--light-colour', '#a7cfdf');
 };
 
-box2.onclick = function() { 
+box2.onclick = function () {
     largeBox[0].className = 'small';
     box2.className = 'large';
+    mainColor.style.setProperty('--main-colour', ' #248c35');
+    mainColor.style.setProperty('--light-colour', '#a6dda8');
 };
 
-box3.onclick = function() { 
+box3.onclick = function () {
     largeBox[0].className = 'small';
     box3.className = 'large';
+    mainColor.style.setProperty('--main-colour', ' #7c2796');
+    mainColor.style.setProperty('--light-colour', '#a4acd8');
 };
 
-box4.onclick = function() { 
+box4.onclick = function () {
     largeBox[0].className = 'small';
     box4.className = 'large';
+    mainColor.style.setProperty('--main-colour', '#23538a');
+    mainColor.style.setProperty('--light-colour', '#a7cfdf');
 };
