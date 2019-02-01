de4ba2483a3cdb50924e0bd7097814fdca6ef587
Gary Robinson <gaz_rob@hotmail.com>
2019-02-01 13:28:55 +0000

    boxmove added to medium box

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 let smallBox = document.getElementsByClassName('small');
+let medBox = document.getElementsByClassName('medium-box');
 let largeBox = document.getElementsByClassName('large');
-const box1 = document.getElementById('box1');
-const box2 = document.getElementById('box2');
+let shownBox = document.getElementsByClassName('medium-show');
 let mainColor = document.documentElement;
-
-
 
 
 box1.onclick = function () {
     largeBox[0].className = 'small';
-    box1.className = 'large';
+    this.className = 'large';
     mainColor.style.setProperty('--main-colour', '#23538a');
     mainColor.style.setProperty('--light-colour', '#a7cfdf');
-
+    shownBox[0].classList.remove("medium-show");
+    medium1.className = 'medium-box medium-show';
 };
 
 box2.onclick = function () {
     largeBox[0].className = 'small';
-    box2.className = 'large';
+    this.className = 'large';
     mainColor.style.setProperty('--main-colour', ' #248c35');
     mainColor.style.setProperty('--light-colour', '#a6dda8');
+    shownBox[0].classList.remove("medium-show");
+    medium2.className = 'medium-box medium-show';
 };
 
 box3.onclick = function () {
     largeBox[0].className = 'small';
-    box3.className = 'large';
+    this.className = 'large';
     mainColor.style.setProperty('--main-colour', ' #7c2796');
     mainColor.style.setProperty('--light-colour', '#a4acd8');
+    shownBox[0].classList.remove("medium-show");
+    medium3.className = 'medium-box medium-show';
 };
 
 box4.onclick = function () {
     largeBox[0].className = 'small';
-    box4.className = 'large';
+    this.className = 'large';
     mainColor.style.setProperty('--main-colour', '#23538a');
     mainColor.style.setProperty('--light-colour', '#a7cfdf');
+    shownBox[0].classList.remove("medium-show");
+    medium4.className = 'medium-box medium-show';
+
 };
